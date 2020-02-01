1fa18bb24ec3e5750dd0bb9bd96ab1d2a9b4d265
Gábor Knizner <knizner@hotmail.com>
2020-02-01 13:26:57 +0100

    Initial commit.

---
+++ OrleansSimpleQueueCacheTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrleansSimpleQueueCacheTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
