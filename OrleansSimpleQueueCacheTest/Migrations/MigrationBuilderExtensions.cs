588a8cd412d77f21d2bef7f7421e760e79c9bfab
Gábor Knizner <knizner@hotmail.com>
2020-02-02 11:42:26 +0100

    Added initial migration to DbContext.

---
+++ OrleansSimpleQueueCacheTest/Migrations/MigrationBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OrleansSimpleQueueCacheTest.Migrations
+{
+    internal static class MigrationBuilderExtensions
+    {
+        public static MigrationBuilder RunSqlScripts(this MigrationBuilder migrationBuilder, params string[] scripts)
+        {
+            foreach (var script in scripts)
+            {
+                var sql = GetResource(script);
+                migrationBuilder.Sql(sql, true);
+            }
+
+            return migrationBuilder;
+        }
+
+        private static string GetResource(string resourceName)
+        {
+            using (var stream = typeof(MigrationBuilderExtensions).Assembly.GetManifestResourceStream($"{typeof(MigrationBuilderExtensions).Namespace}.Sql.{resourceName}"))
+            using (var reader = new StreamReader(stream))
+            {
+                var resource = reader.ReadToEnd();
+                return resource;
+            }
+        }
+    }
+}
